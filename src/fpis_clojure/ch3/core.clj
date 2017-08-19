e0afe54e38bbbc542a736d3215841aaa0ea37aa0
seed.sim <seed.sim@kakaocorp.com>
2017-08-19 12:05:38 +0900

    3.1 solved

--- src/fpis_clojure/ch3/core.clj
+++ src/fpis_clojure/ch3/core.clj
@@ -1,8 +1,23 @@
 (ns fpis-clojure.ch3.core
   (:require [clojure.core.match :refer [match]]))
 
-(defn sum [])
+(defn sum [xs]
+  (match xs
+    [] 0
+    [h & t] (+ h (sum t))))
+  
+(defn product [xs]
+  (match xs
+    [] 1.0
+    [0.0 & r] 0.0
+    [h & t] (* h (product t))))
 
-(defn product [])
-
-;3.1
+; 3.1
+; List matches the third pattern therefore answer is 3
+(let [x [1 2 3 4 5]]
+  (match x
+    [a 2 4 & _] a
+    [] 42
+    [a b 3 4 & _] (+ a b)
+    [h & t] (+ h (sum t))
+    :else (println "failed")))
